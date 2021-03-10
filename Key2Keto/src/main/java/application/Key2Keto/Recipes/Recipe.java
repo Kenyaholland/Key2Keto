70c0e69d7324c254d3a8eef202eb6309684203d0
MBauer-6 <mbauer1015@gmail.com>
2021-03-10 09:30:14 -0600

    inital start to recipe and ingredient classes

--- Key2Keto/src/main/java/application/Key2Keto/Recipes/Recipe.java
+++ Key2Keto/src/main/java/application/Key2Keto/Recipes/Recipe.java
@@ -1,5 +1,29 @@
 package application.Key2Keto.Recipes;
 
-public class Recipe {
+import java.util.ArrayList;
 
+public class Recipe {
+	private String name = "";
+	private ArrayList<Ingredient> ingredients;
+	private int totalCarb = 0;
+	private int totalProtein = 0;
+	private int totalFat = 0;
+	private int ratio = 0;
+	private ArrayList<String> instructions;
+	
+	public Recipe(String name, int carb, int protein, int fat, int ratio) {
+		this.ingredients = new ArrayList<Ingredient>();
+		this.instructions = new ArrayList<String>();
+		this.name = name;
+		this.ratio = ratio;
+		this.totalCarb = carb;
+		this.totalFat = fat;
+		this.totalProtein = protein;
+	}
+	public void addIngredient(Ingredient ing) {
+		this.ingredients.add(ing);
+	}
+	public void addInstruction(String string) {
+		this.instructions.add(string);
+	}
 }
