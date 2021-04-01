5b11bed7dd4faac5d990c8c0e255ba5f1207c607
Kenyaholland <46533825+Kenyaholland@users.noreply.github.com>
2021-04-01 18:12:51 -0500

    Added click listeners, goals field and popUp for confirming user input

--- Key2Keto/src/main/java/application/Key2Keto/Tracker/DayView.java
+++ Key2Keto/src/main/java/application/Key2Keto/Tracker/DayView.java
@@ -1,9 +1,11 @@
 package application.Key2Keto.Tracker;
 
+import application.Key2Keto.Recipes.DayOfWeekView;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart;
@@ -11,6 +13,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Spinner;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -24,57 +27,92 @@ import javafx.scene.text.FontWeight;
 
 public class DayView extends Pane{
 	
-	TextField sleepTextField;
+	Tracker tracker;
+	ConfirmPopUp popUp;
+	String dayOfWeek;
+	
+	Spinner<Double> sleepField;
 	Button sleepButton;
 	Label sleepLabel;
 	
-	TextField waterTextField;
+	Spinner<Double> waterField;
 	Button waterButton;
 	Label waterLabel;
 	
+	TextField goalsTextField;
+	Label goalsLabel;
+	Button addGoalsButton;
+	
 	VBox sleepContent;
 	VBox waterContent;
+	VBox goalsContent;
 	VBox wholeView;
+	
+	String[] styleClasses;
 
 	public DayView(String day){
+		
+		this.dayOfWeek = day;
+		
 		initializeVariables();
 		
-		NumberAxis xaxis = new NumberAxis(1,7,1);  
-        NumberAxis yaxis = new NumberAxis(1,24,1);  
-        xaxis.setLabel("Day of Week");  
-        yaxis.setLabel("Hours of Sleep");  
-            
-        ScatterChart sleepChart = new ScatterChart(xaxis,yaxis);  
-        sleepChart.setTitle("Sleep Overview");  
-             
-        XYChart.Series series = new XYChart.Series();   
-        series.getData().add(new XYChart.Data(1,6));  
-        series.getData().add(new XYChart.Data(2,10));  
-        series.getData().add(new XYChart.Data(3,7));  
-        series.getData().add(new XYChart.Data(4,8));  
-        series.getData().add(new XYChart.Data(5,8));  
-        series.getData().add(new XYChart.Data(6,8));  
-        series.getData().add(new XYChart.Data(7,6));  
- 
-        sleepChart.getData().add(series);  
+        sleepField.getStyleClass().add(Spinner.STYLE_CLASS_SPLIT_ARROWS_HORIZONTAL);
+        waterField.getStyleClass().add(Spinner.STYLE_CLASS_SPLIT_ARROWS_HORIZONTAL);
 		
-		sleepContent.getChildren().addAll(sleepLabel, sleepTextField, sleepButton);
-		waterContent.getChildren().addAll(waterLabel, waterTextField, waterButton);
-		wholeView.getChildren().addAll(sleepContent, waterContent, sleepChart);
+		sleepContent.getChildren().addAll(sleepLabel, sleepField, sleepButton);
+		waterContent.getChildren().addAll(waterLabel, waterField, waterButton);
+		goalsContent.getChildren().addAll(goalsLabel, goalsTextField, addGoalsButton);
+		wholeView.getChildren().addAll(sleepContent, waterContent, goalsContent);
 		this.getChildren().add(wholeView);
 	}
 	
 	private void initializeVariables() {
+		tracker = new Tracker(this.dayOfWeek);
+		popUp = new ConfirmPopUp();
+		
 		wholeView = new VBox();
 		sleepContent = new VBox();
 		waterContent = new VBox();
+		goalsContent = new VBox();
 		
 		sleepLabel = new Label("How many hours of sleep did you have today?");
-		sleepTextField = new TextField();
+		sleepField = new Spinner<Double>(0, 24, 0, 0.5);
 		sleepButton = new Button("Save");
+		sleepButton.setOnAction(new EventHandler<ActionEvent>() {
+		    @Override public void handle(ActionEvent e) {
+		    	if(sleepField.getValue() != null) {
+		    		tracker.setHoursOfSleep(Double.valueOf(sleepField.getValue()));
+		    		popUp.display();
+		    		System.out.println(tracker.getHoursOfSleep());
+		    	}
+		    }
+		});
 		
-		waterLabel = new Label("How many 8 oz glasses of water did you have today?");
-		waterTextField = new TextField();
+		waterLabel = new Label("How many ounces of water did you drink today?");
+		waterField = new Spinner<Double>(0, 128, 0, 1.0);
 		waterButton = new Button("Save");
+		waterButton.setOnAction(new EventHandler<ActionEvent>() {
+		    @Override public void handle(ActionEvent e) {
+		    	if(waterField.getValue() != null) {
+		    		tracker.setWaterIntake(Double.valueOf(waterField.getValue()));	
+		    		popUp.display();
+		    		System.out.println(tracker.getWaterIntake());
+		    	}
+		    }
+		});
+		
+		goalsTextField = new TextField();
+		goalsLabel = new Label("Add your own personalized goals here");
+		addGoalsButton = new Button("Add");
+		addGoalsButton.setOnAction(new EventHandler<ActionEvent>() {
+		    @Override public void handle(ActionEvent e) {
+		    	if(!goalsTextField.getText().isEmpty()) {
+		    		tracker.addGoal(goalsTextField.getText());
+		    		System.out.println(tracker.getGoals());
+		    		goalsTextField.clear();
+		    		popUp.display();
+		    	}
+		    }
+		});
 	}
 }
